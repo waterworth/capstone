f667c6bf537d3a9729afa6d63e6617b33d745a38
Mason Waterworth <hello@masonwaterworth.com>
2020-12-08 03:18:19 -0500

    ✨ Added caching for mequery

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,5 +1,5 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
-import {Request, Response,} from 'express'
+import {Request, Response} from 'express'
 
 
 export type MyContext = { 

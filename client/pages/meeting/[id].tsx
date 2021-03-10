3c158b21379d19e7901dd2145f942ec72b7a365c
Mason Waterworth <hello@masonwaterworth.com>
2021-03-10 13:42:21 -0500

    Update tsconfig

--- client/pages/meeting/[id].tsx
+++ client/pages/meeting/[id].tsx
@@ -1,5 +1,4 @@
 import { useRouter } from 'next/router';
-import React from 'react';
 import styled from 'styled-components';
 import Header from '../../components/Header';
 import Layout from '../../components/Layout';
